d08af7e46203e8e8a207e041a0a1d2c53fa1c52a
Nohus <git@nohus.eu>
2021-09-13 20:36:14 +0100

    Use a range for shareRange

--- src/main/kotlin/com/galarzaa/tibiakt/models/Character.kt
+++ src/main/kotlin/com/galarzaa/tibiakt/models/Character.kt
@@ -24,11 +24,11 @@ data class Character(
     val characters: MutableList<OtherCharacter> = mutableListOf(),
 ) {
 
-    val shareRange: Pair<Int, Int>
+    val shareRange: IntRange
         get() {
             val minLevel = floor((level / 3.0) * 2).toInt()
             val maxLevel = ceil((level / 2.0) * 3).toInt() + if (level % 2 == 0) 1 else 0
-            return Pair(minLevel, maxLevel)
+            return minLevel..maxLevel
         }
 
     val url: String
