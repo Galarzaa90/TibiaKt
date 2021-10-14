64908b0cf2e50ade3930f113d3acbe5aed6f9953
Allan Galarza <allan.galarza@gmail.com>
2021-10-14 14:04:54 -0700

    Added java compatibility

--- publish.gradle.kts
+++ publish.gradle.kts
@@ -13,6 +13,8 @@ configure<SigningExtension> {
 configure<JavaPluginExtension> {
     withJavadocJar()
     withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 afterEvaluate {
