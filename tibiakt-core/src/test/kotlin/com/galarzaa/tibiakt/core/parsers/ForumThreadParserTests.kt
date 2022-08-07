2bb83cc242675efa418852cb37a1522d3d3cf709
Allan Galarza <allan.galarza@gmail.com>
2022-08-07 09:15:54 -0700

    Added back missing test case for recently traded poster

--- tibiakt-core/src/test/kotlin/com/galarzaa/tibiakt/core/parsers/ForumThreadParserTests.kt
+++ tibiakt-core/src/test/kotlin/com/galarzaa/tibiakt/core/parsers/ForumThreadParserTests.kt
@@ -1,10 +1,14 @@
 package com.galarzaa.tibiakt.core.parsers
 
 import com.galarzaa.tibiakt.TestResources.getResource
+import com.galarzaa.tibiakt.core.enums.Vocation
+import com.galarzaa.tibiakt.core.models.forums.ForumAuthor
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAtLeastOne
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 
 class ForumThreadParserTests : StringSpec({
     "Parse thread with traded poster" {
@@ -21,32 +25,30 @@ class ForumThreadParserTests : StringSpec({
         }
     }
 
-    //TODO: Find new sample and update
-//    "Parse thread with a poster that was traded recently" {
-//        val thread =
-//            ForumThreadParser.fromContent(getResource("forums/forumThreadPostAuthorRecentlyTraded.txt"))
-//        thread shouldNotBe null
-//        with(thread!!) {
-//            title shouldBe "Tibia Coin"
-//            threadId shouldBe 4911031
-//            boardId shouldBe 20
-//            currentPage shouldBe 1
-//            totalPages shouldBe 1
-//            resultsCount shouldBe 2
-//            entries shouldHaveSize 2
-//            with(entries.last()) {
-//                author.shouldBeInstanceOf<ForumAuthor>()
-//                with(author as ForumAuthor) {
-//                    title shouldBe "Creature of Habit (Grade 2)"
-//                    isRecentlyTraded shouldBe true
-//                    guild?.name shouldBe "Dark Purples Faster Turtles"
-//                    posts shouldBe 77
-//                }
-//                signature shouldNotBe null
-//
-//            }
-//        }
-//    }
+    "Parse thread with a poster that was traded recently" {
+        val thread =
+            ForumThreadParser.fromContent(getResource("forums/forumThreadPostAuthorRecentlyTraded.txt"))
+        thread shouldNotBe null
+        with(thread!!) {
+            title shouldBe "heffaklumparna!!"
+            threadId shouldBe 4927392
+            boardId shouldBe 25
+            currentPage shouldBe 1
+            totalPages shouldBe 1
+            resultsCount shouldBe 6
+            entries shouldHaveSize 6
+            entries.forAtLeastOne {
+                it.author.shouldBeInstanceOf<ForumAuthor>()
+                with(it.author as ForumAuthor) {
+                    name shouldBe "Wideswing"
+                    world shouldBe "Vunira"
+                    vocation shouldBe Vocation.ELITE_KNIGHT
+                    level shouldBe 159
+                    posts shouldBe 1
+                }
+            }
+        }
+    }
 
     "Parse the content of a thread going to an invalid page" {
         val thread = ForumThreadParser.fromContent(getResource("forums/forumThreadInvalidPage.txt"))
